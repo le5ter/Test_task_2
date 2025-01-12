bc650a4510e524e2b04d42d51c7d3374d5d577f4
le5ter <al.soshnikov@icloud.com>
2025-01-12 15:59:54 +0300

    Add login and logout for user

--- furs_shop/furs_shop/settings.py
+++ furs_shop/furs_shop/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'furs_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,7 +100,11 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
 APPEND_SLASH = False
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
 
 
 # Password validation
