3f0190e80f92b952a0e3095f58d5710f7c04c032
le5ter <al.soshnikov@icloud.com>
2025-01-13 13:13:15 +0300

    Add S3 support

--- furs_shop/furs_shop/settings.py
+++ furs_shop/furs_shop/settings.py
@@ -124,6 +124,18 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
 
+# Настройки для использования S3
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.getenv("S3_REGION_NAME")
+AWS_S3_SIGNATURE_VERSION = os.getenv("S3_SIGNATURE_VERSION")
+AWS_S3_FILE_OVERWRITE = os.getenv("S3_FILE_OVERWRITE")
+AWS_DEFAULT_ACL = os.getenv("AWS_DEFAULT_ACL")
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
